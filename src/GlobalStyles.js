4669cb4d3dca480a6ac4f85b0324d8182ac5cc46
Sascha Collinet <collinet.sascha@gmail.com>
2022-01-06 21:34:21 +0100

    Initial Commit

---
+++ src/GlobalStyles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: inherit;
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+body {
+    min-height: 100vh;
+    font-family: Montserrat, sans-serif; 
+}
+`;
+
+export default GlobalStyles;
